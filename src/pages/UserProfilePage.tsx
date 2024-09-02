25e6ba13f0c1e1be4d828a5131a8f130b03b0888
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-09-02 13:27:55 +0800

    feat: fetching of user information

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,7 +1,23 @@
-import { useUpdateMyuser } from "@/api/MyUserApi";
+import { useGetMyUser, useUpdateMyuser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 export default function UserProfilePage() {
-  const { updateUser, isLoading } = useUpdateMyuser();
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateMyuser();
+
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 }
