2537ffe0320e81288f39700ae7d67bb6700b6c80
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-09-04 11:45:19 +0800

    feat: use update restaurant hook

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,18 +1,24 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } =
+    useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditting = !!restaurant;
 
   return (
     <ManageRestaurantForm
       restaurant={restaurant}
-      onSave={createRestaurant}
-      isLoading={isLoading}
+      onSave={isEditting ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
     />
   );
 }
