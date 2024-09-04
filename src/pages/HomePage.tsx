32b30ea48fdfdce7d4da1a0d6e6597c128e154f2
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-09-04 13:42:38 +0800

    feat: add search bar component and add it to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (searchFormValues: SearchForm) => {
+    navigate({ pathname: `/search/${searchFormValues.searchQuery}` });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div className="bg-white rounder-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
@@ -10,6 +17,10 @@ export default function HomePage() {
           Tuck into a takeway today
         </h1>
         <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSubmit}
+        />
       </div>
       <div className="grid md:grid-cols-2 gap-5">
         <img src={landingImage} />
