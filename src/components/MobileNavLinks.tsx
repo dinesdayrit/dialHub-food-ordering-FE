f55c64e660dec7f503be1b74b05ce96315471d5b
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-09-05 13:03:48 +0800

    feat: add manage restaurant link on mobileNavlinks

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -6,6 +6,10 @@ export default function MobileNavLinks() {
   const { logout } = useAuth0();
   return (
     <>
+      <Link to="/manage-restaurant" className="font-bold hover:text-orange-500">
+        Manage restaurant
+      </Link>
+
       <Link
         to="/user-profile"
         className="flex bg-white items-center font-bold hover:text-orange-500"
