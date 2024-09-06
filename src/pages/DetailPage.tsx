47e45adb67ff6f44c36a565f9e519b6d60d64ff4
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-09-06 16:31:11 +0800

    feat: add Restaurant card info on restau page

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,4 +1,5 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
+import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { useParams } from "react-router-dom";
 
@@ -18,6 +19,11 @@ export default function DetailPage() {
           className="rounded-md object-cover h-full w-full"
         />
       </AspectRatio>
+      <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
+        <div className="flex flex-col gap-4">
+          <RestaurantInfo restaurant={restaurant} />
+        </div>
+      </div>
     </div>
   );
 }
